09f536f08e73dccbcad97b44f2e1f4c351d6917b
mcooper <mikeyjc@gmail.com>
2015-08-24 14:44:19 +1000

    cleaned up JS and added some curved corners

--- app/assets/javascripts/loginScreen.js
+++ app/assets/javascripts/loginScreen.js
@@ -2,14 +2,10 @@ $(document).ready(function() {
   if ($('.loginContainer').length < 0) {
     return;
   }
+  
   console.log('You are on the login screen.');
-
-  // Function hides the overlay and sign-up window.
-  // var hideSignUpOverlay = function() {
-  //   // $('.overlay').hide();
-  //   $('.signUpBox').hide();
-  //   TweenMax.to(".loginBox", 1.2, { left: "50%", ease: Expo.easeOut});
-  // };
+  //Initialise the sign up window size:
+  TweenMax.set('.signUpBox', { scale:.7 }); 
 
   // THE FOLLOWING FOUR FUNCTIONS ARE FOR WHEN THE USER SELECTS THE SIGN UP BUTTON.
   // ------------------------------------------------------------------------------
@@ -57,10 +53,6 @@ $(document).ready(function() {
   $('#closeWindowButton').on('click', shrinkSignUpBox);
 
   // click handler for the new sign up button.
-  
-
-  //Initialise the sign up button function:
-  TweenMax.set('.signUpBox', { scale:.7 }); 
 
 });
 
