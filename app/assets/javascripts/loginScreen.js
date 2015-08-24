aefe17f55b272d337345be4eca25e8e16734eefc
mcooper <mikeyjc@gmail.com>
2015-08-24 14:37:41 +1000

    transition animations working correctly

--- app/assets/javascripts/loginScreen.js
+++ app/assets/javascripts/loginScreen.js
@@ -5,34 +5,62 @@ $(document).ready(function() {
   console.log('You are on the login screen.');
 
   // Function hides the overlay and sign-up window.
-  var hideSignUpOverlay = function() {
-    $('.overlay').hide();
-    $('.signUpBox').hide();
-    TweenMax.to(".loginBox", 1.2, { left: "50%", ease: Expo.easeOut});
+  // var hideSignUpOverlay = function() {
+  //   // $('.overlay').hide();
+  //   $('.signUpBox').hide();
+  //   TweenMax.to(".loginBox", 1.2, { left: "50%", ease: Expo.easeOut});
+  // };
+
+  // THE FOLLOWING FOUR FUNCTIONS ARE FOR WHEN THE USER SELECTS THE SIGN UP BUTTON.
+  // ------------------------------------------------------------------------------
+  var shrinkLoginBox = function() {
+    TweenMax.to('.loginBox', .6 , { scale:.7, ease: Expo.easeOut, onComplete:moveOutLoginBox});
   };
 
-  // Function shows the overlay and sign-up window.
-  var showSignUpOverlay = function() {
-    $('.overlay').show();
-    $('.signUpBox').show();
+  var moveOutLoginBox = function() {
+    TweenMax.to(".loginBox", 1.8, { left: "-20%", ease: Expo.easeOut });
+    moveInSignUpBox();
   };
 
-  // click handler for the 'Sign Up' button.
-  $("#signUpButton").on('click', function() {
-    console.log('you pushed the button');
-    TweenMax.to(".loginBox", 1.2, { left: "12%", ease: Expo.easeOut, onComplete: showSignUpOverlay});
-  });
+  var moveInSignUpBox = function() {
+    TweenMax.to(".signUpBox", 1.8, { left: "50%", ease: Expo.easeOut, onComplete: growSignUpBox });
+  };
 
-  // click handler for the 'overlay' and the 'close window' button in the sign up window, both just hide the overlay and return to the login screen.
-  $('.overlay').on('click', hideSignUpOverlay);
-  $('#closeWindowButton').on('click', hideSignUpOverlay);
+  var growSignUpBox = function() {
+    TweenMax.to(".signUpBox", .6, { scale:1, ease: Expo.easeOut });
+  };
+  // ------------------------------------------------------------------------------
+
+  // THE FOLLOWING FOUR FUNCTIONS ARE FOR WHEN THE USER CLOSES THE SIGN UP WINDOW.
+  // ------------------------------------------------------------------------------
+  var shrinkSignUpBox = function() {
+    TweenMax.to('.signUpBox', .6 , { scale:.7, ease: Expo.easeOut, onComplete:moveOutSignUpBox});
+  };
+
+  var moveOutSignUpBox = function() {
+    TweenMax.to(".signUpBox", 1.8, { left: "120%", ease: Expo.easeOut });
+    moveInLoginBox();
+  };
+
+  var moveInLoginBox = function() {
+    TweenMax.to(".loginBox", 1.8, { left: "50%", ease: Expo.easeOut, onComplete: growLoginBox });
+  };
+
+  var growLoginBox = function() {
+    TweenMax.to(".loginBox", .6, { scale:1, ease: Expo.easeOut });
+  };  
+
+  // ------------------------------------------------------------------------------
+
+  // click handler for the 'Sign Up' button and the 'Close Window' buttons to transition with the correct animations.
+  $("#signUpButton").on('click', shrinkLoginBox);
+  $('#closeWindowButton').on('click', shrinkSignUpBox);
 
   // click handler for the new sign up button.
   
 
   //Initialise the sign up button function:
-  $('.overlay').hide();
-  $('.signUpBox').hide();
+  TweenMax.set('.signUpBox', { scale:.7 }); 
 
 });
 
